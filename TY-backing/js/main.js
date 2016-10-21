4f15d4a0430bb9df9a744e4893a55075e251b347
azhenplus <15163804576@163.com>
2016-10-21 17:51:22 +0800

    充值后台哦，16-10-21
    第二周周五充值后台前端基本页面

--- TY-backing/js/main.js
+++ TY-backing/js/main.js
@@ -1,46 +1,185 @@
 /**
- * Created by Administrator on 2016/10/19.
+ * Created by Administrator on 2016/10/21.
  */
-$().ready(function () {
+$(document).ready(function () {
+    function init() {
 
+        selestTY();//普通用户订单下拉单
+        alertTY();//弹窗
+        modifyTY();////账户管理页面修改权限
+        inputTY();//input  时的各种判断
+    }
     var account = $(".userbox>a");
     var page = $(".paging>li:not('.nextpage')>a");
     var next = $(".paging>li.nextpage>a");
 
+//普通用户订单的下拉框
+    function selestTY() {
+        var select={};
+        select.icon = $(".nav .triangle");//触发按钮
+        select.box=$("ul.order-select");//筛选框
+        select.list=$('ul.order-select>li');//每条筛选条目
+        select.content=select.list.text();//每条筛选条目的具体内容
+
+        select.icon.bind("mouseenter",function () {
+            select.box.slideDown("slow",function () {
+
+            });
+        });
+
+        select.box.bind('mouseleave',function () {
+            $(this).slideUp(2000);
+        });
+
+        select.list.bind('click',function () {
+            $(this).addClass("active").siblings("li").removeClass("active");
+            select.box.slideUp(2000);
+        });
+    }
+
 //弹窗
-    var alert={};
-    alert.out = $(".output-btn");//导出订单按钮
-    alert.alertbox=$(".alert-box");//弹窗盒子
-    alert.log=$(".account-info .changelog");//修改登录密码按钮
-    alert.logalertbox=$(".login-box");//修改登录密码的盒子
-    alert.pay=$(".account-info .changepay");//修改支付密码的盒子
-    alert.payalertbox=$(".pay-box");//修改支付密码的盒子
-    alert.confirm=$(".alertbox,.login-box,.pay-box .yes-btn");//弹窗的确定按钮
+    function alertTY() {
+        var alert={};
+        alert.out = $(".mainbox .output-btn");//导出订单按钮
+        alert.agentout=$(".mainbox .agent-output-btn");//经销商用户界面的导出按钮
+        alert.alertbox=$(".alert-box");//弹窗盒子
+        alert.agentbox=$(".agent-alert-box");//经销商用户的弹窗盒子
 
-    alert.out.bind("click",function () {
-        alert.alertbox.show();
-    });
+        alert.log=$(".account-info .changelog");//修改登录密码按钮
+        alert.logalertbox=$(".login-box");//修改登录密码的盒子
 
-    alert.confirm.bind("click",function () {
-        alert.alertbox.hide();
-        alert.logalertbox.hide();
-        alert.payalertbox.hide();
-    });
+        alert.pay=$(".account-info .changepay");//修改支付密码的盒子
+        alert.payalertbox=$(".pay-box");//修改支付密码的盒子
 
-    alert.log.bind('click',function () {
-        alert.logalertbox.show();
-    });
+        alert.adminbox=$(".alert-admin-box");//输入管理员密码弹窗
+        alert.moneybox=$(".alert-money-box");//打款弹窗
+        alert.paying=$("table .orange .paying");//表格中打款按钮
+        alert.moneybtn=$(".alert-admin-box .money-btn");//激发打款弹窗的按钮
 
-    alert.pay.bind('click',function () {
-        alert.payalertbox.show();
-    });
+        alert.close =$(".close-btn");//关闭弹窗的按钮
+        alert.confirm=$(".yes-btn");//弹窗的确定按钮
 
-//用户管理页面的切换
-    account.bind('click', function (e) {
-        e.preventDefault();
-        $(this).toggleClass("active")
-            .siblings("a").removeClass("active")
-    });
+        alert.paying.bind("click",function () {
+
+            alert.adminbox.show();
+
+        });
+
+        alert.moneybtn.bind("click",function () {
+            // $.ajax({
+            //     //发送管理员密码判断是否正确
+            // });
+            alert.moneybox.show();
+        });
+        alert.out.bind("click",function () {
+            alert.alertbox.show();
+        });
+
+        alert.agentout.bind("click",function () {
+            alert.moneybox.hide();
+            alert.adminbox.hide();
+            alert.agentbox.show();
+        });
+
+        alert.close.bind('click',function () {
+            alert.alertbox.hide();
+            alert.logalertbox.hide();
+            alert.payalertbox.hide();
+        });
+        alert.confirm.bind("click",function () {
+            alert.alertbox.hide();
+            alert.logalertbox.hide();
+            alert.payalertbox.hide();
+        });
+
+        alert.log.bind('click',function () {
+            alert.logalertbox.show();
+        });
+
+        alert.pay.bind('click',function () {
+            alert.payalertbox.show();
+        });
+    }
+//账户管理页面修改权限
+    function modifyTY() {
+        var modify={};
+        modify.modify = $(".admin-list .modify");//修改权限按钮
+        modify.box = $(".modify-box");//修改权限的弹出盒子
+        modify.icon = $(".modify-box .triangle");//下拉按钮三角
+        modify.lists = $(".modify-box .modify-lists");//下拉列表
+        modify.li = $(".modify-box .modify-lists>li");//下拉的每一项
+        modify.newli=$(".modify-box .newli");//已选择的每一项
+        modify.confirm=$(".modify-box .yes-btn");//确定按钮
+        modify.delete =$(".modify-box .no");//删除已选择的选项
+        modify.close =$(".modify-box .close-btn");//关闭弹窗
+        modify.modify.bind('click',function () {
+            modify.box.show();
+        });
+
+        modify.icon.bind('click',function () {
+            modify.lists.slideToggle();
+        });
+
+        modify.li.bind("click",function () {
+            var n=$(this).index();
+            $(this).addClass("active");
+            modify.newli[n].style.display="block";
+        });
+
+        modify.delete.bind("click",function () {
+            $(this).parents(".newli").hide();
+            var n=$(this).parents(".newli").index()-1;
+            var li = $(modify.li[n]);
+            li.removeClass("active");
+        });
+        modify.confirm.bind("click",function () {
+            modify.box.hide();
+        });
+
+        modify.close.bind("click",function () {
+            modify.box.hide();
+        });
+    }
+
+//公共弹窗
+    var tip=["输入不能为空!"];
+
+    function myAlert(tip) {
+        var $commanalert=$(" <div class='comman-alert'></div>");
+        var $alertheader=$(" <div class='alert-header'>!! 重要提示</div>");
+        var $alertbody=$("<div class='alert-body'></div>" );
+        var $p=$("<p>"+tip+"</p>");
+        var $yes=$("<div class='yes-btn'>确定</div>");
+        $alertbody.append($p);
+        $alertbody.append($yes);
+        $commanalert.append($alertheader);
+        $commanalert.append($alertbody);
+        $("header").after($commanalert);
+
+        var commanalert={};
+        commanalert.confirm=$(".comman-alert .yes-btn"); //确定按钮
+        commanalert.box=$(".comman-alert");//弹窗盒子
+
+        commanalert.confirm.bind("click",function () {
+            commanalert.box.hide();
+        });
+    }
+
+
+
+//input的判断
+    function inputTY() {
+        var input={};
+        input.text=$("input[type='text']");//文本输入框
+        input.tel= $("input[type='tel']");//电话输入框
+        // input.text.on("blur",function () {
+        //     if ($(this).val()==""){
+        //         myAlert(tip[0]);
+        //     }
+        // });
+    }
+
+
 
     //页码切换
 
@@ -53,8 +192,10 @@ $().ready(function () {
 
 
     next.bind("click", function () {
-     $(".paging>li.active").removeClass("active")
-         .next("li:not('.next')").addClass("active");
+        $(".paging>li.active").removeClass("active")
+            .next("li:not('.next')").addClass("active");
 
-        })
+    });
+
+    init();
 });
